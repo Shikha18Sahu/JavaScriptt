ad8370618b5cfba72cf9d28e0593a9338f6f4cc1
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-05-07 13:27:01 +0530

    Refactor comments and clarify data types in datatypes.js
    Updated comments and added explanations for data types.

--- 01-basics/02-datatypes.js
+++ 01-basics/02-datatypes.js
@@ -1,7 +1,7 @@
 "use strict";  // treat all JS code as newer version
-
-//  alert(3+3)  //
-//  code readability should be high
+// Strict mode enables a stricter set of rules for JavaScript, which can help catch common coding mistakes and improve the overall quality of your code.
+alert("Hello, World!"); // This will work in non-strict mode, but in strict mode, it will throw a ReferenceError because 'alert' is not defined as a global variable.
+// Bcs We are using nodejs, not browser
 
 console.log(3+3) 
 console.log("Shikha");
@@ -11,7 +11,10 @@ console.log("Shikha");
 let name= "Shikha"
 let age= 18
 let isLoggedIn = false
-let state;
+let state; // undefined
+let state= null; // 0 
+
+//PRimitive Datatypes
 
 // number => range: 2 to pwer 53
 // bigint
@@ -21,8 +24,11 @@ let state;
 // undefined => abhi value assigned nai hui hai
 // symbol => unique
 
+// Non Primitive(reference) Datatypes
+// object // array // function
 
-// object 
+console.log(typeof null);  //Object  // a historical bug in JavaScript.
+console.log(typeof arr);  //Object  
+console.log(typeof function);  //Object  
 
-console.log(typeof null);  //Object  
 console.log(typeof undefined);  //undefined
