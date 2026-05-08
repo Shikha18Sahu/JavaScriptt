d6e8357aad8ca773c286aa76847c9dacf5556996
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-05-08 12:52:07 +0530

    Refactor string examples and add comments
    Updated string manipulation examples and added comments explaining methods.

--- 01-basics/05_strings.js
+++ 01-basics/05_strings.js
@@ -1,38 +1,43 @@
 const name = "shikha"
 const repocount = 50
-
-// console.log(name + repocount + " Value");
+// console.log(name + repocount + " Value");   // shikha50 Value
 
 console.log(`Hello my name is ${name} and my repo count is ${repocount}`) ; //modern way
 
-const gameName = new String("shikha-sahu")
-
-console.log(gameName[2]);    //i
-console.log(gameName.__proto__);  //{}
-
-
-
-console.log(gameName.length);   //10
-console.log(gameName.toUpperCase());   //SHIKHASAHU
-console.log(gameName);         //[String: "shikhasahu"]  
+const gameName = new String("shikha-sahu") // object
+console.log(gameName[2]);    //i fro, shikha-sahu
+console.log(gameName.__proto__);  // {}
+console.log(gameName.length);   //11
+console.log(gameName.toUpperCase());   //SHIKHA-SAHU
+console.log(gameName);         //[String: "shikha-sahu"]  
 console.log(gameName.charAt(3));   //k
-console.log(gameName.indexOf('a'));   //k
+console.log(gameName.indexOf('a')); //5
+
+
+// Interview Point
+// slice() does NOT modify original string
+// It returns new string.
+// Strings are immutable in JS.
+
+// One More Important Difference
+// substring() -> ❌ negative values support nahi karta.
+// slice() ->✅ negative indexing support karta hai.
 
 const newString = gameName.substring(0,4)
-console.log(newString);
+console.log(newString);   //shik
 
-const anotherString = gameName.slice(-9,4)
-console.log(anotherString);
+const anotherString = gameName.slice(-9,4) // length : 11 =>   11-9 = 2 => (2,4)
+console.log(anotherString);  //ik
 
-const newStringOne = "      shikha    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+const newStringOne = "      shikha    " // whitespace
+console.log(newStringOne);    //     shikha     //
+console.log(newStringOne.trim());   //shikha
 
 const url = "https://hitesh.com/shikha20%sahu"
-console.log(url.replace('20%', '-'));
-console.log(url.includes('shikha'))
+console.log(url.replace('20%', '-'));    //https://hitesh.com/shikha-sahu
+console.log(url.includes('shikha'))     //TRUE
 
-console.log(gameName.split('-'))
+console.log(gameName.split('-'))  //['shikha', 'sahu']
 
 
 //Go to all method on mdn website 
