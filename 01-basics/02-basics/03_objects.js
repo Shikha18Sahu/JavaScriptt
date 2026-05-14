94fc6e0c20b6ce4eb9aef9a616df608fd5b55bed
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-05-14 11:50:24 +0530

    edited

--- 01-basics/02-basics/03_objects.js
+++ 01-basics/02-basics/03_objects.js
@@ -1,42 +1,35 @@
-//singleton
-//ObjectCreate
+//singleton-- cunstructor se
+//Object.create
 
 // object literals
-
 const mySym = Symbol("key1")
-
-
 const JsUser = {
     name: "Shikha",
     "full name": "Shikha Sahu",
-    [mySym]: "khushi", // brackts refers to Symbol
+    [mySym]: "khushi", // square brackts refers to Symbol
     age: 18,
     location: "Lucknow",
     email: "shikha@google.com",
     isLoggedIn: false, 
     lastLoginDays: ["Monday", "Saturday"]
-
 }
+console.log(JsUser.email)   // shikha@google.com  // not a right way generally
+console.log(JsUser["email"])   // shikha@google.com
+console.log(JsUser["full name"])   // Shikha Sahu   //.se access nhi kr skte
+console.log(JsUser[mySym])   // khushi .
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["full name"])
-console.log(JsUser[mySym])
-
-JsUser.email = "shikha@google.com"
-// Object.freeze(JsUser)   // now jsUser are not changable
+JsUser.email = "shikha@google.com" 
+Object.freeze(JsUser)   // now jsUser are not changable // values are lock 
 JsUser.email = "Shikha@microsoft.com"
-console.log(JsUser);
-
-
+console.log(JsUser);   //shikha@google.com
 
 JsUser.greeting = function(){
   console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-  console.log(`Hello JS user, ${this.name}`);
+  console.log(`Hello JS user, ${this.name}`); // same object ko reference krn aho to this 
 }
-console.log(JsUser.greeting);
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting);  // function (anonymous)
+console.log(JsUser.greeting());    // Hello JS user
+console.log(JsUser.greetingTwo());    // Hello JS user, Shikha 
 
