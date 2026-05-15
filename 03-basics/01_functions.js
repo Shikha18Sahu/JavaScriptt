1d2a715349ee8f4da22252d03ad94ffb84d6f41b
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-05-15 12:56:26 +0530

    Refactor functions and add new features

--- 03-basics/01_functions.js
+++ 03-basics/01_functions.js
@@ -1,6 +1,4 @@
 // function with object and Array
-
-
 function sayMyName(){
   console.log("S")
   console.log("H")
@@ -13,7 +11,6 @@ function sayMyName(){
 // sayMyName();    //execute the function 
 // sayMyName();
 
-
 // function addTwoNumbers(number1, number2){  //parameters
 //   console.log(number1 + number2)
 // }
@@ -25,83 +22,69 @@ function sayMyName(){
 // addTwoNumbers("4",4)
 
 
-
 //Important
-function addTwoNumbers(number1, number2){ 
+function addTwoNumbers(number1, number2){     // parameters
   console.log(number1 + number2)
 }
-const result = addTwoNumbers(3,5)
-
-
+const result = addTwoNumbers(3,5)   // Arguement
 console.log("RESULT: " , result);   //undefined as after calling addTwoNumbers it only print not return the value so result have not value to store so it show undefined.
 
 
-// function addTwoNumbers(number1, number2){ 
+function addTwoNumbers(number1, number2){ 
+  return (number1 + number2)
+  console.log("Shikha");  //not print as after return 
+}
 
-//   // let result = number1 + number2
-//   // return result
-//   return (number1 + number2)
-  
-//   // console.log("Shikha");  //not print as after return 
-  
-// }
-// // const result = addTwoNumbers(3,5)
-
-// // console.log("RESULT: " , result); 
+ const result = addTwoNumbers(3,5)
+console.log("RESULT: " , result);   // 8
 
 // //Jab function me return; likhte hain bina kisi value ke, toh JavaScript automatically undefined return karti hai.
 // // aur agr username ko funtion ke andr ek value fixed kr doge to undefined nai show hoga bin value ke vo show hoga as KHUSI
 
 
-
-// // function loginUserMessage(username = "khuhsi"){
-// //   if(!username){    //jab usename nai diya hoga
-// //     //  console.log("PLease enter a username");
-// //      return     //function yhin return ho jayega (undefined return krega)
-// //   }
-// //   return `${username} just logged in `
-// // }
-
-// // console.log(loginUserMessage("Shikha"))
-// // console.log(loginUserMessage())    //undefined just logged in
+function loginUserMessage(username = "khuhsi"){
+ if(!username){    //jab usename nai diya hoga
+ console.log("PLease enter a username");
+  return     //function yhin return ho jayega (undefined return krega)
+ }
+  return `${username} just logged in `
+}
+ console.log(loginUserMessage("Shikha"))
+console.log(loginUserMessage())    //undefined just logged in
 
 
+function calculateCartPrice(...num1) {  //rest Operator
+   return num1   // return in array
+}
+console.log(calculateCartPrice(9,8,2)) [9,8,2]  // where more than one value pass => rest operator ...nums1
 
-// function calculateCartPrice(...num1) {  //rest Operator
-//    return num1   // return in array
-// }
+function calculateCartPrice(val1 , val2, ...num1) {  //rest Operator
+  return num1   // return in array
+}
+console.log(calculateCartPrice(9,8,2,4))  // in num1 : 2,4  
 
-// // console.log(calculateCartPrice(9,8,2))
+const user = {
+  username: "hitesh", 
+  price : 99
+}
 
+function handleObject (anyobject){
+  console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);  // undefined on the case of not matching variables
+}
+handleObject(user)
 
-// function calculateCartPrice(val1 , val2, ...num1) {  //rest Operator
-//   return num1   // return in array
-// }
+handleObject({
+  username: "sam", 
+  price: 399
+})
 
-// // console.log(calculateCartPrice(9,8,2,4))  // in num1 : 2,4  
+const myNewArray = [ 200,300,400]
 
-// const user = {
-//   username: "hitesh", 
-//   price : 99
-// }
+function returnSecondValue(getArray){
+  return getArray[1]  // 300
+}
 
-// function handleObject (anyobject){
-//   // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);  // undefined on the case of not matching variables
-// }
-
-// // handleObject(user)
-// handleObject({
-//   username: "sam", 
-//   price: 399
-// })
-
-// const myNewArray = [ 200,300,400]
-
-// function returnSecondValue(getArray){
-//   return getArray[1]
-// }
-
-// // console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue(myNewArray));
 
 
 
